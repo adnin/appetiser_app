f2259b745006357689be063e7c67053f9005241d
adnin <adninonofre@gmail.com>
2021-12-02 00:48:58 +0800

    Feature/config (#2)
    * add store config
    
    * add common file

--- src/common/api.service.js
+++ src/common/api.service.js
@@ -1,3 +1,4 @@
+import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import JwtService from "@/common/jwt.service";
@@ -16,13 +17,13 @@ const ApiService = {
   },
 
   query(resource, params) {
-    return Vue.axios.get(resource, params).catch((error) => {
+    return Vue.axios.get(resource, params).catch(error => {
       throw new Error(`[Appetiserdev] ApiService ${error}`);
     });
   },
 
   get(resource, slug = "") {
-    return Vue.axios.get(`${resource}/${slug}`).catch((error) => {
+    return Vue.axios.get(`${resource}/${slug}`).catch(error => {
       throw new Error(`[Appetiserdev] ApiService ${error}`);
     });
   },
@@ -40,7 +41,7 @@ const ApiService = {
   },
 
   delete(resource) {
-    return Vue.axios.delete(resource).catch((error) => {
+    return Vue.axios.delete(resource).catch(error => {
       throw new Error(`[Appetiserdev] ApiService ${error}`);
     });
   }
