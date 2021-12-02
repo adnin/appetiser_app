fe772fd1172f1dd8ca2ce835a7cfc4f0d380dd1a
Adnin Onofre <adninonofre@gmail.com>
2021-12-02 08:30:33 +0800

    start e2e testing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -31,10 +31,9 @@
             type="submit"
             class="flex absolute right-0 top-0 mt-5 mr-5"
           >
-            <span class="font-bold mr-1">Logout</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 m-r-1 text-red-600 hover:text-red-500"
+              class="h-6 w-6 mr-1 text-red-600 hover:text-red-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -53,10 +52,9 @@
             @click="login"
             class="flex absolute right-0 top-0 mt-5 mr-5"
           >
-            <span class="font-bold mr-1">Login</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 m-r-1 text-blue-600 hover:text-blue-500"
+              class="h-6 w-6 mr-1 text-blue-600 hover:text-blue-500"
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
@@ -68,6 +66,7 @@
                 d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
               />
             </svg>
+
             <span class="font-bold">Login</span>
           </button>
         </div>
